78f4838cfe644fb154138bae0d5f95709da222ad
MarianaSko <maryana.skorynovych@gmail.com>
2024-01-03 22:50:58 +0200

    v-2

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,13 +1,16 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+interface PageInterface{
+  title:string
+}
 
 class Component<T> {
   constructor (public props:T) {
   }
 }
 
-class Page extends Component<{title:string}> {
+class Page extends Component<PageInterface> {
   pageInfo () {
     console.log(this.props.title);
   }
